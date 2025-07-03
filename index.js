b7934bdcc3f71b45af03846726285b4abab8aa4d
tornik2 <tornik2@yahoo.com>
2025-07-03 15:37:07 +0400

    first ts repo

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("lets doe");
+const one = 12;
+const anotherONe = 223;
+console.log(one + anotherONe);
---
+++ index.ts
@@ -0,0 +1,5 @@
+console.log("lets doe");
+const one = 12;
+const anotherONe = 223;
+
+console.log(one + anotherONe);
